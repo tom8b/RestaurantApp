0b3cbda246b1234ced8a023bc8db7954e08ebacc
Tomasz <tomek8b@gmail.com>
2019-03-06 17:30:16 +0100

    Added migration 'initial create'

---
+++ RestaurationApp/Migrations/20190306162633_Initial Create.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RestaurationApp.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cuisines",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CuisineName = table.Column<string>(nullable: true),
+                    CuisineDescription = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cuisines", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reservations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    NumberOfGuests = table.Column<int>(nullable: false),
+                    FirstName = table.Column<string>(nullable: true),
+                    SecondName = table.Column<string>(nullable: true),
+                    TelephoneNumber = table.Column<string>(nullable: true),
+                    MailAddress = table.Column<string>(nullable: true),
+                    ReservationDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reservations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TypesOfDishes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TypesOfDishes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RestaurantTables",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    NumberOfChairs = table.Column<int>(nullable: false),
+                    ReservationId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RestaurantTables", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RestaurantTables_Reservations_ReservationId",
+                        column: x => x.ReservationId,
+                        principalTable: "Reservations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Dishes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    DishName = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Cost = table.Column<decimal>(nullable: false),
+                    CuisineId = table.Column<int>(nullable: true),
+                    TypeOfDishId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dishes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Dishes_Cuisines_CuisineId",
+                        column: x => x.CuisineId,
+                        principalTable: "Cuisines",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Dishes_TypesOfDishes_TypeOfDishId",
+                        column: x => x.TypeOfDishId,
+                        principalTable: "TypesOfDishes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dishes_CuisineId",
+                table: "Dishes",
+                column: "CuisineId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dishes_TypeOfDishId",
+                table: "Dishes",
+                column: "TypeOfDishId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RestaurantTables_ReservationId",
+                table: "RestaurantTables",
+                column: "ReservationId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Dishes");
+
+            migrationBuilder.DropTable(
+                name: "RestaurantTables");
+
+            migrationBuilder.DropTable(
+                name: "Cuisines");
+
+            migrationBuilder.DropTable(
+                name: "TypesOfDishes");
+
+            migrationBuilder.DropTable(
+                name: "Reservations");
+        }
+    }
+}
